2e1671a455f24ecd56d580432ee2ccae687f94f3
ReaI IOU <vladimirsv@gmail.com>
2022-11-26 23:02:44 +0600

    game

--- app/main.py
+++ app/main.py
@@ -26,9 +26,10 @@ app.config.LOGGING = True
 
 app.extend(config=Config(
     cors_supports_credentials=True,
-    cors_methods="GET, POST, OPTIONS",
-    cors_origins='*',
-    cors_allow_headers = "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time"
+    # cors_methods="GET, POST, OPTIONS",
+    cors_methods="*",
+    # cors_origins='*',
+    cors_allow_headers="Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time"
 )
 )
 
