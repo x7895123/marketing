109ea183412f2ed16a6baa13c48d36fd8e015d0f
x7895123 <vladimirsv@gmail.com>
2022-12-13 13:19:50 +0600

    game

--- app/app/routes/models/bill.py
+++ app/app/routes/models/bill.py
@@ -1,12 +1,14 @@
+import datetime
+
 class Payment:
-    payment_type = str
-    payment_type_id = int
+    payment_type = 'cash'
+    payment_type_id = 0
     payment = 1.1
 
 
 class Bill:
-    company_bill_id = str
-    paytime = str
-    amount = float
+    company_bill_id = '123'
+    paytime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S +6')
+    amount = 1.1
     phone = '+77010000864'
-    # payment = Payment
+    payment = Payment
