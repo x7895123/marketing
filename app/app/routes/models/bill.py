8f6b53f0290262aec6eabade97b74aa3e2ee9ac0
x7895123 <vladimirsv@gmail.com>
2022-12-13 13:41:59 +0600

    game

--- app/app/routes/models/bill.py
+++ app/app/routes/models/bill.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 class Payment:
     payment_type = 'cash'
     payment_type_id = 0
@@ -11,4 +12,4 @@ class Bill:
     paytime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S +6')
     amount = 1.1
     phone = '+77010000864'
-    payment = Payment
+    payment = [Payment]
