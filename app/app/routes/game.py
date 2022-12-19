0e5c5ed2318d9cf198933978ed92a70e141f461b
x7895123 <vladimirsv@gmail.com>
2022-12-19 14:23:40 +0600

    game

--- app/app/routes/game.py
+++ app/app/routes/game.py
@@ -7,6 +7,7 @@ from sanic import text, exceptions, json
 from sanic.log import logger
 from sanic import Blueprint
 
+from .spin_items.items import items
 from ..models import bills
 from ..routes.login import verify_password
 
@@ -93,66 +94,8 @@ async def get_items(request):
         if not await verify_password(request=request):
             return text(f"Basic Authentication error", status=400)
 
-        items = [
-            {
-                "icon": "items/aquaCash",
-                "label": "АкваCash 1000",
-                "amount": 1000,
-                "chance": 2,
-                "tokenId": 37
-            },
-            {
-                "icon": "items/aquaday",
-                "label": "Аква Day 1",
-                "amount": 1,
-                "chance": 23,
-                "tokenId": 63
-            },
-            {
-                "icon": "items/aquaCash",
-                "label": "АкваCash 750",
-                "amount": 750,
-                "chance": 5,
-                "tokenId": 37
-            },
-            {
-                "icon": "items/barrel",
-                "label": "Barrel",
-                "amount": 1,
-                "chance": 10,
-                "tokenId": 65
-            },
-            {
-                "icon": "items/aquaCash",
-                "label": "АкваCash 500",
-                "amount": 501,
-                "chance": 10,
-                "tokenId": 37
-            },
-            {
-                "icon": "items/fitness",
-                "label": "Fitness",
-                "amount": 1,
-                "chance": 15,
-                "tokenId": 60
-            },
-            {
-                "icon": "items/aquaCash",
-                "label": "АкваCash 300",
-                "amount": 301,
-                "chance": 15,
-                "tokenId": 37
-            },
-            {
-                "icon": "items/aquaday_kids",
-                "label": "Aquaday kids",
-                "amount": 1,
-                "chance": 30,
-                "tokenId": 64
-            },
-        ]
-        logger.info(f"get_items: {items}")
-        return json(items)
+        logger.info(f"get_items: {items.get(request.ctx.company)}")
+        return json(items.get(request.ctx.company))
     except Exception as e:
         logger.error(f'{inspect.stack()[0][1]} {inspect.stack()[0][3]}: {e}')
         return json(f"get_spin error", status=400)
---
+++ app/app/routes/spin_items/items.py
@@ -0,0 +1,73 @@
+items = {
+    "city": [
+        {"icon": "items/aquaday", "label": "aquaday1", "amount": 1, "chance": 15, "tokenId": 63},
+        {"icon": "items/Koin", "label": "Koin1000", "amount": 1000, "chance": 5, "tokenId": 66},
+        {"icon": "items/arena", "label": "arena1000", "amount": 1000, "chance": 5, "tokenId": 2},
+        {"icon": "items/Koin", "label": "Koin750", "amount": 750, "chance": 10, "tokenId": 66},
+        {"icon": "items/aquaday", "label": "aquaday1", "amount": 1, "chance": 15, "tokenId": 63},
+        {"icon": "items/arena", "label": "arena750", "amount": 750, "chance": 10, "tokenId": 2},
+        {"icon": "items/Koin", "label": "Koin500", "amount": 500, "chance": 15, "tokenId": 66},
+        {"icon": "items/arena", "label": "arena500", "amount": 500, "chance": 15, "tokenId": 2},
+        {"icon": "items/Koin", "label": "Koin300", "amount": 300, "chance": 10, "tokenId": 66},
+    ],
+
+    "aqua": [
+        {
+            "icon": "items/aquaCash",
+            "label": "АкваCash 1000",
+            "amount": 1000,
+            "chance": 2,
+            "tokenId": 37
+        },
+        {
+            "icon": "items/aquaday",
+            "label": "Аква Day 1",
+            "amount": 1,
+            "chance": 23,
+            "tokenId": 63
+        },
+        {
+            "icon": "items/aquaCash",
+            "label": "АкваCash 750",
+            "amount": 750,
+            "chance": 5,
+            "tokenId": 37
+        },
+        {
+            "icon": "items/barrel",
+            "label": "Barrel",
+            "amount": 1,
+            "chance": 10,
+            "tokenId": 65
+        },
+        {
+            "icon": "items/aquaCash",
+            "label": "АкваCash 500",
+            "amount": 501,
+            "chance": 10,
+            "tokenId": 37
+        },
+        {
+            "icon": "items/fitness",
+            "label": "Fitness",
+            "amount": 1,
+            "chance": 15,
+            "tokenId": 60
+        },
+        {
+            "icon": "items/aquaCash",
+            "label": "АкваCash 300",
+            "amount": 301,
+            "chance": 15,
+            "tokenId": 37
+        },
+        {
+            "icon": "items/aquaday_kids",
+            "label": "Aquaday kids",
+            "amount": 1,
+            "chance": 30,
+            "tokenId": 64
+        },
+    ]
+
+}
